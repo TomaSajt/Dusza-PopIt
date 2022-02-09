cb445c33e94099940ffcea695cf30d26c17797e5
Zsong Lee <zsongli@gmail.com>
2022-02-09 11:56:30 +0100

    Rectangle struct that uses shorts

---
+++ PopIt/Data/Rectangle.cs
@@ -0,0 +1,12 @@
+﻿namespace PopIt.Data
+{
+    record struct Rectangle(short X, short Y, short Width, short Height)
+    {
+        public short Top { get => Y; }
+        public short Bottom { get => (short)(Y + Height); }
+        public short Left { get => X; }
+        public short Right { get => (short)(X + Width); }
+
+        public bool Contains(short x, short y) => x >= Left && x <= Right && y >= Top && y <= Bottom;
+    }
+}
